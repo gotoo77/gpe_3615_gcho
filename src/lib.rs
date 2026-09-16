3156550087a81db41e7e82b4dc2b53001960fc8b
gotoo <gautier.duval@gmail.com>
2026-09-16 14:39:02 +0200

    style: apply rustfmt to MFE-6 exports

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,7 @@ mod sound;
 
 pub use app::GchoApp;
 pub use content::{ContentBundle, ContentFile, EditorialMessage};
-pub use experience::{
-    LiveServicePulse, TerminalTiming, public_service_slogan, selection_hint,
-};
+pub use experience::{LiveServicePulse, TerminalTiming, public_service_slogan, selection_hint};
 pub use facade::{
     FunctionKey, FunctionKeySpec, function_key_at, function_keys, snapshot_date_label,
 };
