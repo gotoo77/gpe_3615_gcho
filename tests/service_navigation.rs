60e0bfcc705eccf823f6b84a78bfb1c4af399717
gotoo <gautier.duval@gmail.com>
2026-09-16 08:32:27 +0200

    test: define MFE-1 detail navigation contract

--- tests/service_navigation.rs
+++ tests/service_navigation.rs
@@ -1,4 +1,4 @@
-use gpe_3615_gcho::{NavCommand, PageId, Service};
+use gpe_3615_gcho::{DetailId, NavCommand, PageId, Service};
 
 #[test]
 fn required_pages_are_available() {
@@ -50,4 +50,66 @@ fn return_and_summary_follow_minitel_navigation() {
     assert_eq!(service.current_page(), PageId::Noeud7);
     service.apply(NavCommand::Summary);
     assert_eq!(service.current_page(), PageId::Accueil);
+}
+
+fn open_page(service: &mut Service, digit: u8) {
+    service.apply(NavCommand::Digit(digit));
+    service.apply(NavCommand::Send);
+}
+
+#[test]
+fn key_services_open_real_detail_screens() {
+    let mut service = Service::new();
+
+    open_page(&mut service, 1);
+    open_page(&mut service, 1);
+    assert_eq!(service.current_detail(), Some(DetailId::ArcadeScores));
+
+    service.apply(NavCommand::Summary);
+    open_page(&mut service, 2);
+    open_page(&mut service, 1);
+    assert_eq!(service.current_detail(), Some(DetailId::Mailbox));
+
+    service.apply(NavCommand::Summary);
+    open_page(&mut service, 3);
+    open_page(&mut service, 2);
+    assert_eq!(service.current_detail(), Some(DetailId::ServicePublic));
+
+    service.apply(NavCommand::Summary);
+    open_page(&mut service, 4);
+    open_page(&mut service, 3);
+    assert_eq!(service.current_detail(), Some(DetailId::GpeProjects));
+
+    service.apply(NavCommand::Summary);
+    open_page(&mut service, 5);
+    open_page(&mut service, 1);
+    assert_eq!(service.current_detail(), Some(DetailId::Node7File));
+}
+
+#[test]
+fn return_closes_detail_before_leaving_its_service_page() {
+    let mut service = Service::new();
+    open_page(&mut service, 3);
+    open_page(&mut service, 2);
+    assert_eq!(service.current_page(), PageId::Infos);
+    assert_eq!(service.current_detail(), Some(DetailId::ServicePublic));
+
+    service.apply(NavCommand::Return);
+    assert_eq!(service.current_page(), PageId::Infos);
+    assert_eq!(service.current_detail(), None);
+
+    service.apply(NavCommand::Return);
+    assert_eq!(service.current_page(), PageId::Accueil);
+}
+
+#[test]
+fn summary_clears_detail_state() {
+    let mut service = Service::new();
+    open_page(&mut service, 5);
+    open_page(&mut service, 1);
+    assert_eq!(service.current_detail(), Some(DetailId::Node7File));
+
+    service.apply(NavCommand::Summary);
+    assert_eq!(service.current_page(), PageId::Accueil);
+    assert_eq!(service.current_detail(), None);
 }
